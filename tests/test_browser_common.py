7460ab3df9af5e5ff859ec955f99c3bd07a62695
Hemanth <hemanthkavula@gmail.com>
2026-09-20 22:42:18 -0400

    Remove abandoned post-queue application automation

--- tests/test_browser_common.py
+++
@@ -1,6 +0,0 @@
-from app.browser.common import is_sensitive
-
-def test_sensitive_questions():
-    assert is_sensitive("Will you now or in the future require sponsorship?")
-    assert is_sensitive("Desired salary")
-    assert not is_sensitive("LinkedIn URL")
