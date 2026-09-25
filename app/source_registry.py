68e430e4d2aa18dab3e89a9dde271d15b8810bda
Hemanth <hemanthkavula@gmail.com>
2026-09-25 16:33:38 -0400

    Expand niche workforce and education source recognition

--- app/source_registry.py
+++ app/source_registry.py
@@ -100,13 +100,13 @@ PATTERNS={
  "ceipal":[r"https?://[^/]*ceipal\\.com/",r"https?://jobs\\.ceipal\\.com/"],
  "trakstar_hire":[r"https?://[^/]*hire\\.trakstar\\.com/",r"https?://hire\\.trakstar\\.com/jobs/"],
  "neogov":[r"https?://[^/]*governmentjobs\\.com/",r"https?://[^/]*neogov\\.com/",r"https?://www\\.governmentjobs\\.com/careers/[^/]+/jobs/\\d+"],
- "schooljobs":[r"https?://[^/]*schooljobs\\.com/"],
- "higheredjobs":[r"https?://[^/]*higheredjobs\\.com/"],
- "applynow":[r"https?://[^/]*applynow\\.net/"],
+ "schooljobs":[r"https?://[^/]*schooljobs\\.com/",r"https?://www\\.schooljobs\\.com/careers/[^/]+/jobs/\\d+"],
+ "higheredjobs":[r"https?://[^/]*higheredjobs\\.com/",r"https?://www\\.higheredjobs\\.com/admin/details\.cfm"],
+ "applynow":[r"https?://[^/]*applynow\\.net/",r"https?://[^/]*apply-now\\.(?:com|net)/"],
  "talentreef":[r"https?://[^/]*talentreef\\.com/",r"https?://apply\\.talentreef\\.com/"],
  "icims_alt":[r"https?://[^/]*icims\\.com/jobs/"],
- "jobappnetwork":[r"https?://[^/]*jobappnetwork\\.com/"],
- "myworkchoice":[r"https?://[^/]*myworkchoice\\.com/"],
+ "jobappnetwork":[r"https?://[^/]*jobappnetwork\\.com/",r"https?://apply\\.jobappnetwork\\.com/"],
+ "myworkchoice":[r"https?://[^/]*myworkchoice\\.com/",r"https?://jobs\\.myworkchoice\\.com/"],
  "ultipro_ukg":[r"https?://[^/]*ultipro\\.com/"],
 }
 
